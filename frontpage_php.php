c4d289f8da01ee899bb142c7abff64211c94b7ef
Taha Aslam <Zohaibrasool30@yahoo.com>
2022-05-19 21:10:06 +0500

    signup backend update

---
+++ frontpage_php.php
@@ -0,0 +1,31 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database_name = "car_rental";
+
+    $conn = mysqli_connect($servername, $username, $password, $database_name);
+
+    if (!$conn){
+        die("Connection Failed: ". mysqli_connect_error());
+    }
+
+    if(isset($_POST['save'])){
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+
+        $sql_query = "INSERT INTO user (username, email, password, confirm_password)
+                    VALUES ('$username', '$email', '$gender', '$password', '$confirm_password')";
+        
+        if (mysqli_query($conn, $sql_query)){
+            echo "DETAILS ENTERED";
+        }
+
+        else{
+            echo "ERROR: ", $sql, " ", mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    }
+?>
