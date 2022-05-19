95814554a610fedc8801f92865e68680b5e2d40c
Taha Aslam <Zohaibrasool30@yahoo.com>
2022-05-19 22:52:40 +0500

    Update frontpage_php.php

--- frontpage_php.php
+++ frontpage_php.php
@@ -14,10 +14,9 @@
         $username = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
-        $confirm_password = $_POST['confirm_password'];
         $password_encrypted = password_hash($password, PASSWORD_DEFAULT);
 
-        $sql_query = "INSERT INTO user (username, email, password, confirm_password) VALUES ('$username', '$email', '$password_encrypted', '$confirm_password')";
+        $sql_query = "INSERT INTO user (username, email, password, confirm_password) VALUES ('$username', '$email', '$password_encrypted')";
         
         if (mysqli_query($conn, $sql_query)){
             echo "DETAILS ENTERED";
