55218c258a7dda78db63711d825248e44c5a2b64
Aliyaali1 <aliyaaliraza5@gmail.com>
2022-05-22 17:47:34 +0500

    new

---
+++ homepage_admin.php
@@ -0,0 +1,53 @@
+<?php
+  // Create database connection
+  $db = mysqli_connect("localhost", "root", "", "image_upload");
+
+  // Initialize message variable
+  $msg = "";
+
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+  	$image = $_FILES['image']['name'];
+  	// Get text
+  	$image_text = mysqli_real_escape_string($db, $_POST['image_text']);
+
+  	// image file directory
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+  	// execute query
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM images");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Image Upload</title>
+    <link rel="stylesheet" href="homepage_admin.css">
+</head>
+<body>
+    <div id="content">
+        <form method="post" action="homepage_admin.php" enctype="multipart/form-data">
+            <input type="hidden" name="size" value="1000000">
+            <div>
+                <input type="file" name="image">
+            </div>
+            <div>
+                <textarea name="text" cols="40" rows="4" placeholder="Say something about this Car"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="upload" value="upload image">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
